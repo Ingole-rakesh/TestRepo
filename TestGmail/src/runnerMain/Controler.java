a7fbada59c3488821cbe44624e1b2798d9efa02f
Ingole-rakesh <ingole.rakesh@gmail.com>
2016-09-04 02:01:13 -0500

    Update Controler.java
    ok

--- TestGmail/src/runnerMain/Controler.java
+++ TestGmail/src/runnerMain/Controler.java
@@ -8,7 +8,7 @@ public class Controler {
 
 	WebDriver driver = null;
 	ReadXcel xcel = new ReadXcel();
-	System.out.println("");
+	System.out.println("changes made in here to verify pull at client in IDE");
 	
 	
 }
