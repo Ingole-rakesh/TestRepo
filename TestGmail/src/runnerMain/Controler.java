275b7337f92536dd2adab2b67c40621184224e6f
Rakesh <Rakesh@192.168.1.7>
2016-09-04 01:54:29 -0500

    commiting all file to Remote

---
+++ TestGmail/src/runnerMain/Controler.java
@@ -0,0 +1,14 @@
+package runnerMain;
+
+import org.openqa.selenium.WebDriver;
+
+import manipulateXcel.ReadXcel;
+
+public class Controler {
+
+	WebDriver driver = null;
+	ReadXcel xcel = new ReadXcel();
+	
+	
+	
+}
---
+++ TestGmail/src/runnerMain/UIOperation.java
@@ -0,0 +1,5 @@
+package runnerMain;
+
+public class UIOperation {
+
+}
